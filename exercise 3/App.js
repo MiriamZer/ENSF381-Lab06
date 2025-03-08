fd7fbd43babf79d14592ff925ef33374d60bcf25
MiriamZer <miriam.zeresenai@ucalgary.ca>
2025-03-07 18:33:26 -0700

    Update App.js

--- exercise 3/App.js
+++ exercise 3/App.js
@@ -4,7 +4,6 @@ import EngineeringTopics from './EngineeringTopics';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Engineering</h1>
       <EngineeringTopics /> 
     </div>
   );
